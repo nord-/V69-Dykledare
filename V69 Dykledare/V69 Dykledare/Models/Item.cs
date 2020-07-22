e410440d9c89f48ee1a3284d6a2e306b5b1fdb1b
Rickard Nord <rickard.nord@combitech.se>
2020-07-22 12:18:42 +0200

    Add project files.

---
+++ V69 Dykledare/V69 Dykledare/Models/Item.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace V69_Dykledare.Models
+{
+    public class Item
+    {
+        public string Id { get; set; }
+        public string Text { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ V69 Dykledare/V69 Dykledare/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using V69_Dykledare.Models;
+
+namespace V69_Dykledare.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
